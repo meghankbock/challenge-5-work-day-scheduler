849562f3304bcebf4ee6d7d132836c69f0e8edce
Meghan Bock <meghan.bock@gmail.com>
2021-06-14 00:24:37 -0500

    added comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,12 @@ var eventObj = {
   row16: [],
   row17: [],
 };
-//var eventArr = [];
 
-// check current time and update timeblocks to present/past/future
+// check current time and update timeblocks to present/past/future + current date
 var updateTimeBlocks = function () {
-  console.log("time blocks updated");
   currentTime = moment().format("H");
 
+  // assign classes based on past, current, and future
   for (var i = 0; i < timeBlockArray.length; i++) {
     var timeBlockItem = $("#box-" + timeBlockArray[i] + "");
     timeBlockItem.classList = "description col-sm-10";
@@ -32,52 +31,64 @@ var updateTimeBlocks = function () {
     }
   }
 
+  // render current date at top
   var currentDay = moment().format("dddd MMMM DD, YYYY");
   var spanEl = $("#date");
   spanEl.text(currentDay);
 };
 
+// blur event handler
 var blurTextHandler = function (event) {
   var targetEl = $(event.target);
   var parentId = targetEl.closest(".time-block").attr("id");
   var textBoxId = parentId.replace("row", "box");
   var eventText = $("#" + textBoxId + "").val();
-  var closestBtn = targetEl.closest('.time-block').children('.saveBtn');
-  var unsavedSpanEl = targetEl.closest('.time-block').children('.unsaved-text');
-  console.log("unsavedSpanEl: " + unsavedSpanEl);
   var parentListId = targetEl
     .closest(".time-block")
     .attr("id")
     .replace("-", "");
+
+  // enable delay before identifying active element
   setTimeout(function () {
     var activeEl = document.activeElement;
+
+    // active element is html body
+    if (activeEl.matches("body")) {
+      targetEl.addClass("unsaved");
+      return;
+    }
+
+    // active element is not html body
     var activeParentId = activeEl.closest(".time-block").getAttribute("id");
-    
-    /*if (targetEl.is("button")) {
-        textBoxEl.removeClass("unsaved");
-        console.log("class removed");
-    }*/
+
+    // check it prior element and active element parents match and existing event text saved
     if (parentId != activeParentId && eventObj[parentListId].length !== 0) {
+      // different parents and text has changed > add unsaved class
       if (eventText !== eventObj[parentListId][0].text) {
         targetEl.addClass("unsaved");
-        console.log("class added");
-      } else {
-        targetEl.removeClass("unsaved");
-        console.log("class removed");
       }
-    } 
-    else if (parentId != activeParentId && eventObj[parentListId].length === 0 && eventText !== "") { 
-        targetEl.addClass("unsaved");
-        unsavedSpanEl.text("Unsaved Changes");
-        console.log("class added");
-    }
-    else {
+      // remove unsaved class
+      else {
         targetEl.removeClass("unsaved");
-        console.log("class removed");
+      }
+    }
+    // different parents, no existing event text, and event text not blank
+    else if (
+      parentId != activeParentId &&
+      eventObj[parentListId].length === 0 &&
+      eventText !== ""
+    ) {
+      // add unsaved class
+      targetEl.addClass("unsaved");
+    }
+    // remove unsaved class
+    else {
+      targetEl.removeClass("unsaved");
     }
   }, 1);
 };
 
+// save button handler
 var saveButtonHandler = function (event) {
   var targetEl = $(event.target);
   var parentId = targetEl.closest(".time-block").attr("id");
@@ -89,6 +100,7 @@ var saveButtonHandler = function (event) {
   var eventText = $("#" + textBoxId + "").val();
   var textBoxEl = $("#" + textBoxId + "");
 
+  // event text empty
   if (!eventText) {
     if (eventObj[parentListId].length === 0 || !eventObj[parentListId]) {
       alert("You have not entered an event description.");
@@ -97,28 +109,36 @@ var saveButtonHandler = function (event) {
     textBoxEl.removeClass("unsaved");
     saveEvents();
     return;
-  } else if (eventText) {
+  }
+  // create new event description
+  else if (eventText) {
     if (eventObj[parentListId].length === 0 || !eventObj[parentListId]) {
       eventObj[parentListId] = [];
       eventObj[parentListId].push({
         text: eventText,
         id: textBoxId,
-        status: "saved",
       });
-    } else {
+    }
+    // update existing event description
+    else {
       eventObj[parentListId][0].text = eventText;
     }
   }
+  // save events to local storage
   saveEvents();
+  // remove unsaved class
   textBoxEl.removeClass("unsaved");
 };
 
-//
+// create event
 var createEvent = function (eventText, textBoxId) {
   var textBoxEl = $("#" + textBoxId + "");
+
+  // add event text to corresponding time block row
   textBoxEl.text(eventText);
 };
 
+// load events from local storage
 var loadEvent = function () {
   eventObj = JSON.parse(localStorage.getItem("events"));
 
@@ -136,19 +156,18 @@ var loadEvent = function () {
       row16: [],
       row17: [],
     };
-    console.log("no events in local storage");
     return false;
   }
 
-  // loop over object properties
+  // loop over object properties and add event text to page
   $.each(eventObj, function (list, arr) {
     arr.forEach(function (event) {
       createEvent(event.text, event.id);
-      //eventArr.push(event);
     });
   });
 };
 
+// save events to local storage
 var saveEvents = function () {
   localStorage.setItem("events", JSON.stringify(eventObj));
 };
@@ -164,7 +183,8 @@ setInterval(function () {
   updateTimeBlocks();
 }, 1000 * 60 * 5);
 
-// event on save
+// event on save button click
 $(".saveBtn").click(saveButtonHandler);
 
+// blur event on textarea
 $("textarea").blur(blurTextHandler);
